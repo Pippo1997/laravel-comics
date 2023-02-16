d97442dd06e418ec67921924f1e2f2459a50c8d1
Filippo Sbaiz <sbaizfilippo@gmail.com>
2023-02-16 21:35:55 +0100

    creazione route per hader main footer

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,26 @@ Route::get('/', function () {
     return view('Home');
 });
 
+// ROUTE PER HEADER
+Route::get('/header', function () {
+    // recupero array menu
+    $menu = config('comics.menu');
+    return view('header', compact('menu'));
+});
+
+// ROUTE PER MAIN
+Route::get('/main', function () {
+    // recupero array fumetti
+    $fumetti = config('comics.fumetti');
+    return view('main', compact('fumetti'));
+});
+
+// ROUTE PER FOOTER
+Route::get('/footer', function () {
+    // recupero array icone
+    $icone = config('comics.icone');
+    // recupero array social
+    $social = config('comics.social');
+    return view('footer', compact('icone','social'));
+});
+
